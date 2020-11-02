cfbec04feb827a5a438c2e84f439ee9ad440261e
Josiah Davis <josiah.j.davis@gmail.com>
2020-11-02 16:20:53 +0800

    shifting python from plit --> src/plit

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name="plit",
-    packages=find_packages(where="plit"),
-    package_dir={"": "plit"},
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     version="0.1.0",
     description="""A distribution of publication quality, static, ML-specific,
     data visualization templates built on Matplotlib.""",
