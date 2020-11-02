f1760db1efaa3401cdfcef6eb0c3208167e3aaca
Josiah Davis <josiah.j.davis@gmail.com>
2020-11-02 16:53:47 +0800

    adding some metadata to the setup incl keywords and classifiers

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import find_packages, setup
+import os
 import versioneer
+
+
+def read(fname):
+    """Read content on file name."""
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
     name="plit",
@@ -7,9 +14,23 @@ setup(
     package_dir={"": "src"},
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    description="""A distribution of publication quality, static, ML-specific,
-    data visualization templates built on Matplotlib.""",
+    description="A wrapper for automating common matplotlib tasks",
+    long_description=read("README.md"),
+    url="https://github.com/awslabs/plit",
+    keywords="visualization data science analytics analysis matplotlib",
     author="Josiah Davis",
     license="Apache 2.0",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.6.0",
     install_requires=["matplotlib"],
 )
